bba74f7667d2a34253ebdc80e4fb981b7fecb58e
CFHM <hcdunn88@gmail.com>
2021-11-20 22:21:47 -0600

    bunny stops at ledges using flip sprite

--- Assets/Scripts/Bunny.cs
+++ Assets/Scripts/Bunny.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Bunny : MonoBehaviour
 {
 
-    private enum BunnyStates { Idle, Alert, Move};
+    private enum BunnyStates { Idle, Alert, Move };
 
     //config params
     [SerializeField] float moveSpeed = 10f;
@@ -13,32 +13,107 @@ public class Bunny : MonoBehaviour
     [SerializeField] float attackRange = 2f;
     [SerializeField] float minFidgetTimer = 0.5f;
     [SerializeField] float maxFidgetTimer = 3f;
+    [SerializeField] bool patrols;
+    [SerializeField] float minPatrolTimer = 0.5f;
+    [SerializeField] float maxPatrolTimer = 1f;
 
     //cached references
+    Rigidbody2D myRigidBody;
     Animator myAnimator;
     SpriteRenderer myRenderer;
     GameObject currentTarget;
     [SerializeField] BunnyStates myState;
     float fidgetTimer;
-    
+    float patrolTimer;
+    [SerializeField] float hopAnimationTime;
+    bool hopping = false;
+
     // Start is called before the first frame update
     void Start()
     {
+        myRigidBody = GetComponent<Rigidbody2D>();
         myAnimator = GetComponent<Animator>();
         myRenderer = GetComponent<SpriteRenderer>();
         myState = BunnyStates.Idle;
         fidgetTimer = Random.Range(minFidgetTimer, maxFidgetTimer);
+        if (patrols)
+        {
+            patrolTimer = Random.Range(minPatrolTimer, maxPatrolTimer);
+            AnimationClip[] clips = myAnimator.runtimeAnimatorController.animationClips;
+            foreach (AnimationClip clip in clips)
+            {
+                switch(clip.name)
+                {
+                    case "Bunny Hop":
+                        hopAnimationTime = clip.length;
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        Patrol();
         CheckForCharacters();
         MoveToCharacter();
         AnimateBunny();
     }
 
-    private void AnimateBunny()
+    private void Patrol()
+    {
+        if (!patrols) { return; }
+        if (myState == BunnyStates.Idle)
+        {
+            if (patrolTimer <= 0)
+            {
+                StartCoroutine(HopOnce());
+            }
+            else
+            {
+                patrolTimer -= Time.deltaTime;
+            }
+        }
+        if (myState == BunnyStates.Move)
+        {
+            transform.Translate(moveSpeed * Time.deltaTime * ((myRenderer.flipX.GetHashCode() * 2) -1), 0, 0);
+            CheckForWall();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        myRenderer.flipX = !myRenderer.flipX;
+    }
+
+
+    private void CheckForWall()
+    {
+        RaycastHit2D foundWall = Physics2D.Raycast(transform.position, Vector2.left, 0.1f);
+        if (foundWall == false) { return; }
+        if (foundWall.collider.gameObject.layer == 8)
+        {
+            myRenderer.flipX = !myRenderer.flipX;
+        }
+        else
+        { return; }
+    }
+
+    private IEnumerator HopOnce()
+    {
+        myState = BunnyStates.Move;
+        hopping = true;
+        yield return new WaitForSeconds(hopAnimationTime);
+        patrolTimer = Random.Range(minPatrolTimer, maxPatrolTimer);
+        myState = BunnyStates.Idle;
+        hopping = false;
+    }
+
+
+private void AnimateBunny()
     {
         switch (myState)
         {
@@ -66,7 +141,7 @@ public class Bunny : MonoBehaviour
 
     private void CheckForCharacters()
     {
-        
+        if (hopping) { return; }
         if (currentTarget != null) { return; }
         RaycastHit2D foundTarget = Physics2D.Raycast(transform.position, Vector2.left, awareRange);
         if (foundTarget == false) 
